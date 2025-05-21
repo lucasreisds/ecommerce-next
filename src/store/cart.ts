209aef80f6527e85c62ddf0f145a7f231d911679
Lucas Reis <lucasrdsmarley@gmail.com>
2025-05-21 16:15:53 -0300

    feat: zustand store

---
+++ src/store/cart.ts
@@ -0,0 +1,15 @@
+import { create } from 'zustand'
+
+type Product = { id: number; title: string; price: number }
+
+type CarState = {
+    items: Product[]
+    add: (item: Product) => void
+    remove: (id: number) => void
+}
+
+export const useCart = create<CarState>((set) => ({
+    items: [],
+    add: (item) => set((state) => ({ items: [...state.items, item] })),
+    remove: (id) => set((state) => ({ items: state.items.filter((item) => item.id !== id) }))
+}))
