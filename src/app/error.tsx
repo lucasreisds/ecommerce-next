70f42c5d53ba67f60cb66a9bfcbe42131f949a19
Lucas Reis <lucasrdsmarley@gmail.com>
2025-05-21 22:23:25 -0300

    feat: added error page

---
+++ src/app/error.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+export default function Error({ error }: { error: Error }) {
+    return (
+        <div className="text-center p-10">
+            <h1 className="text-2xl font-bold text-red-600">Algo deu errado</h1>
+            <p className="mt-2 text-gray-700">{error.message}</p>
+        </div>
+    )
+}
--- src/components/AddToCartButton.tsx
+++ src/components/AddToCartButton.tsx
@@ -6,7 +6,8 @@ export function AddToCartButton({ product }: { product: StoreProduct }) {
     const add = useCart((state) => state.add);
 
     return (
-        <button onClick={() => add(product)} className="mt-4 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition cursor-pointer">
+        <button onClick={() => add(product)}
+            className="mt-4 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition cursor-pointer">
             Add to Cart
         </button>
     );
