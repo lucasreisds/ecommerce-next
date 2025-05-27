c1f73f0b4fc3090e53b5d128c54b8ef47ac3ba97
Lucas Reis <lucasrdsmarley@gmail.com>
2025-05-27 15:20:41 -0300

    fix: changed message to english

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -3,7 +3,7 @@
 export default function Error({ error }: { error: Error }) {
     return (
         <div className="text-center p-10">
-            <h1 className="text-2xl font-bold text-red-600">Algo deu errado</h1>
+            <h1 className="text-2xl font-bold text-red-600">Something went wrong</h1>
             <p className="mt-2 text-gray-700">{error.message}</p>
         </div>
     )
